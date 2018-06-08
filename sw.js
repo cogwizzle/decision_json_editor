be2b7e3481c0ed4fb8c6e2d28d2be9273d4ded4d
Joe Fehrman <joseph.fehrman@gmail.com>
2018-06-07 21:22:22 -0400

    Updated bad app.js build.

--- sw.js
+++ sw.js
@@ -2,15 +2,9 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open('decision_tree').then(function (cache) {
       return cache.addAll([
-<<<<<<< HEAD:sw.js
-        '/decision_json_editor/',
-        '/decision_json_editor/index.html',
-        '/decision_json_editor/decision_json_editor.js'
-=======
         './',
         './index.html',
         './app.js'
->>>>>>> 7329a19065d59201ebeecd67c5b919362312fc03:src/sw.js
       ]);
     })
   );
